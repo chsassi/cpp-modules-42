845febfd176521d1cdb37f71a18e844863ec2cf9
Christian Sassi <chrisassi@macbook.Home>
2024-11-26 10:16:38 +0100

    transfer

--- cpp00/ex01/phonebook.cpp
+++ cpp00/ex01/phonebook.cpp
@@ -1,1 +1,26 @@
 #include "phonebook.hpp"
+
+
+
+int    ChooseMode()
+{
+    std::string Mode;
+
+    if (Mode != "1" || Mode != "ADD" || Mode != "2" || Mode != "SEARCH" || Mode != "EXIT")
+        return std::cout << "Invalid input!" << std::endl, 0;
+    else
+    {
+        
+    }
+}
+
+int main(int ac, char **av)
+{
+    std::string Mode;
+
+    std::cout << "Welcome to PhoneBook!\nChoose mode!" << std::endl;
+    std::cout << "1 or ADD\n2 or SEARCH\n3 or EXIT" << std::endl;
+    
+    std::cin >> Mode;
+    ChooseMode()
+}
--- cpp00/ex01/phonebook.hpp
+++ cpp00/ex01/phonebook.hpp
@@ -3,4 +3,18 @@
 
 # include <iostream>
 
+class Contact
+{
+    std::string FirstName;
+    std::string SecondName;
+    std::string NickName;
+    std::string PhoneNumber;
+    std::string DarkSecret;
+};
+
+class PhoneBook
+{
+    Contact contact[8];
+};
+
 #endif
